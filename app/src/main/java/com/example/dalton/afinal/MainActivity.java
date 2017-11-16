2a16e840cb5d41e7343b6571365d5e3465bbcdd8
Dalton Neely <djneely@mavs.coloradomesa.edu>
2017-11-16 15:26:01 -0700

    Added button bindings to MainActivity.java

--- app/src/main/java/com/example/dalton/afinal/MainActivity.java
+++ app/src/main/java/com/example/dalton/afinal/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dalton.afinal;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button oneButton = (Button) findViewById(R.id.oneButton);
+        Button twoButton = (Button) findViewById(R.id.twoButton);
+        Button threeButton = (Button) findViewById(R.id.threeButton);
+        Button fourButton = (Button) findViewById(R.id.fourButton);
+        Button fiveButton = (Button) findViewById(R.id.fiveButton);
+        Button sixButton = (Button) findViewById(R.id.sixButton);
+        Button sevenButton = (Button) findViewById(R.id.sevenButton);
+        Button eightButton = (Button) findViewById(R.id.eightButton);
+        Button nineButton = (Button) findViewById(R.id.nineButton);
+        
     }
 }
