4f8b9ef2ac1b9e59376811c03714dbd85f56dc30
Dalton Neely <djneely@mavs.coloradomesa.edu>
2017-11-16 15:50:06 -0700

    added Event handler to oneButton

--- app/src/main/java/com/example/dalton/afinal/MainActivity.java
+++ app/src/main/java/com/example/dalton/afinal/MainActivity.java
@@ -4,16 +4,27 @@ import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View.OnClickListener input = new View.OnClickListener(){
+        public void onClick(View v){
+            TextView textbox = (TextView) findViewById(R.id.textbox);
+            textbox.setText("1");
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button oneButton = (Button) findViewById(R.id.oneButton);
+        oneButton.setOnClickListener(input);
         Button twoButton = (Button) findViewById(R.id.twoButton);
         Button threeButton = (Button) findViewById(R.id.threeButton);
         Button fourButton = (Button) findViewById(R.id.fourButton);
@@ -22,6 +33,6 @@ public class MainActivity extends AppCompatActivity {
         Button sevenButton = (Button) findViewById(R.id.sevenButton);
         Button eightButton = (Button) findViewById(R.id.eightButton);
         Button nineButton = (Button) findViewById(R.id.nineButton);
-        
+
     }
 }
